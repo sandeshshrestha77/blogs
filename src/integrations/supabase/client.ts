4692a314566781f8631da7397b788082b5073b54
Sandesh Shrestha <sandeshstha67@gmail.com>
2025-02-24 15:41:47 +0545

    Update client.ts

--- src/integrations/supabase/client.ts
+++ src/integrations/supabase/client.ts
@@ -1,7 +1,7 @@
 import { createClient } from '@supabase/supabase-js';
 import type { Database } from './types';
 
-const SUPABASE_URL = process.env.NEXT_PUBLIC_SUPABASE_URL!;
-const SUPABASE_ANON_KEY = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY!;
+const SUPABASE_URL = process.env.REACT_APP_SUPABASE_URL!;
+const SUPABASE_ANON_KEY = process.env.REACT_APP_SUPABASE_ANON_KEY!;
 
 export const supabase = createClient<Database>(SUPABASE_URL, SUPABASE_ANON_KEY);
