60c75f139f2f87b0b9185e6b63c9351c99aa0129
Sandesh Shrestha <sandeshstha67@gmail.com>
2025-03-06 15:03:49 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -3,12 +3,10 @@ import react from "@vitejs/plugin-react-swc";
 import path from "path";
 import { componentTagger } from "lovable-tagger";
 
-// https://vitejs.dev/config/
 export default defineConfig(({ mode }) => ({
   server: {
-    host: "::",
+    host: "0.0.0.0",
     port: 8080,
-    allowedHosts: ["1a74adcf-a89c-4962-81a3-01fbfe0a6f4c.lovableproject.com.blogs-5t79.onrender.com"], // Added allowedHosts
   },
   plugins: [
     react(),
