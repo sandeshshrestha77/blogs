b20242a75bd0f4f7f4ee2927f631fe189b919c94
Sandesh Shrestha <sandeshstha67@gmail.com>
2025-03-26 10:03:22 +0545

    Update vite.config.ts

--- vite.config.ts
+++ vite.config.ts
@@ -7,6 +7,7 @@ export default defineConfig(({ mode }) => ({
   server: {
     host: "0.0.0.0",
     port: 8080,
+    allowedHosts: ["1a74adcf-a89c-4962-81a3-01fbfe0a6f4c.lovableproject.com"]
   },
   plugins: [
     react(),
